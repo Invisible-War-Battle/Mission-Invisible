b1d2f7ab6697b7eb7ef9ba94624527136d6584f9
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-07-24 12:49:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ document.getElementById("universe").hidden = true;
 document.getElementById("text").hidden = true;
 document.body.style.background = "black";
 document.getElementById("option").innerHTML = "Do You Want To Create A Room?";
-
+Swal.getContainer().style.height = "2000px";
 document.getElementById("ok").onclick = async () => {
 	const { value: room } = await Swal.fire({
 		customClass: {
